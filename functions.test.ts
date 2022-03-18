1c5e624983e2186a1e6965461a3eb636c7b02fbe
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-03-18 17:19:28 -0600

    fixed my tests, everything up and running

--- functions.test.ts
+++ functions.test.ts
@@ -2,17 +2,18 @@ const {shuffleArray} = require('./utils')
 
 let testData = [1, 2, 3, 4]
 
-
 describe('shuffleArray should', () => {
-    // CODE HERE
+    
+
     test("should return an array of the same length as the argument", () => {
         let testArr = shuffleArray(testData)        
         expect(testArr.length).toEqual(testData.length); 
     }); 
 
-    // test("should return data of type array", () => {
-    //     let test1 = shuffleArray(testData)
-    //     expect(test1.isArray).toBe(true)
-    // }); 
+    test("should return data of type array", () => {
+        let  test1 = shuffleArray(testData)
+        let result = Array.isArray(test1)
+        expect(result).toEqual(true)
+    }); 
 
 })
